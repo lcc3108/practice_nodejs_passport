55760664da25d9ec2b9165c6740be5a56c85e910
Chaecheol Im <dlacocjf78@naver.com>
2019-08-13 00:53:59 +0900

    terraform test

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const helloGET = (req, res) => {
+    return res.send('hello');
+};
+
+export default true;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(j|t)s$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        babelrc: true
+                    }
+                }
+            }
+        ]
+    }
+};
