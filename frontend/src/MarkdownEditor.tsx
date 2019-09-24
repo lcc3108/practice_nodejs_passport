7c48e0f31ff8e87663443f7f3f5672514d0c1b90
Chaecheol Im <38449269+lcc3108@users.noreply.github.com>
2019-09-25 00:10:06 +0900

    Update MarkdownEditor.tsx

--- frontend/src/MarkdownEditor.tsx
+++ frontend/src/MarkdownEditor.tsx
@@ -14,19 +14,19 @@ interface IMarkdownEditorState {
 class MarkdownEditor extends React.Component<IMarkdownEditorProps, IMarkdownEditorState> {
   public state = { value: "" };
 
-  handleChange(value: string) {
-    this.setState({
-      value,
-    });
-  }
-
-  render() {
+  public render() {
     const { value } = this.state;
     return (
       <Dialog open={this.props.dialogOpen} onClose={this.props.handleClose} aria-labelledby="makrdown-editor" fullScreen disableEscapeKeyDown={true}>
         <Editor value={value} onChange={this.handleChange.bind(this)} placeholder={"# 마크다운"} />
       </Dialog>
     );
+  }
+
+  private handleChange(value: string) {
+    this.setState({
+      value,
+    });
   }
 }
 
