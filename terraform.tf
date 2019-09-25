ebbf029e875cedc1ee8a75fd64f01b35ec182e82
Chaecheol Im <38449269+lcc3108@users.noreply.github.com>
2019-09-25 23:01:43 +0900

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -75,23 +75,22 @@ resource "google_app_engine_standard_app_version" "version_id" {
     }
   }
 }
+
 # AWS SETTING
+# provider "aws" {
+#   region                  = "us-west-2"
+#   shared_credentials_file = "awskey.csv"
+# }
 
-provider "aws" {
-  region                  = "us-west-2"
-  shared_credentials_file = "awskey.csv"
-}
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "terraform"
+# }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "terraform"
-}
-
-resource "aws_lambda_function" "aws_function" {
- filename = "${data.archive_file.backend_zip.output_path}"
- function_name="test"
-   source_code_hash = "${filebase64sha256("./backend.zip")}"
- runtime = "nodejs8.10"
-   handler       = "exports.test"
-   role= "${aws_iam_role.la}"
-
-}
+# resource "aws_lambda_function" "aws_function" {
+#  filename = "${data.archive_file.backend_zip.output_path}"
+#  function_name="test"
+#    source_code_hash = "${filebase64sha256("./backend.zip")}"
+#  runtime = "nodejs8.10"
+#    handler       = "exports.test"
+#    role= "${aws_iam_role.la}"
+# }
