599c0ea7af35ca9c437ea3ced32c4966db27bd81
pjainxido <pjane123@naver.com>
2019-09-29 02:26:59 +0900

    Update markdown

--- frontend/components/MarkdownEditor.tsx
+++ frontend/components/MarkdownEditor.tsx
@@ -1,4 +1,4 @@
-import Editor from "for-editor";
+//import Editor from "for-editor";
 import React from "react";
 import { Dialog } from "@material-ui/core";
 
@@ -18,7 +18,7 @@ class MarkdownEditor extends React.Component<IMarkdownEditorProps, IMarkdownEdit
     const { value } = this.state;
     return (
       <Dialog open={this.props.dialogOpen} onClose={this.props.handleClose} aria-labelledby="makrdown-editor" fullScreen disableEscapeKeyDown={true}>
-        <Editor value={value} onChange={this.handleChange.bind(this)} placeholder={"# 마크다운"} language={"en"} subfield={true} preview={true} />
+        {/* {/* <Editor value={value} onChange={this.handleChange.bind(this)} placeholder={"# 마크다운"} language={"en"} subfield={true} preview={true} /> */} */}
       </Dialog>
     );
   }
